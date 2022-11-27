cc3ca545d5d60a5a2e8fdfaf1cdc99241b9cdb63
Vladimir Kazantsev <gordiancrew@gmail.com>
2022-11-27 21:59:37 +0300

    fix: arrays task process

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -279,8 +279,16 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  let resArr = [];
+  arr.map((item, index) => {
+    const currentArrey = new Array(index + 1);
+
+    currentArrey.fill(item);
+    resArr = resArr.concat(currentArrey);
+    return item;
+  });
+  return resArr;
 }
 
 
@@ -297,8 +305,8 @@ function propagateItemsByPositionIndex(/* arr */) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  return arr.slice(-3).reverse();
 }
 
 
